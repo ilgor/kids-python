947b3cb315978b4690518283d00c21b22e416883
ilgor <ilgorbek.kuchkarov@pearson.com>
2019-12-25 00:15:27 -0500

    minor update

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bolalar uchun sodda <code>Python</code> kurslari kutib oling!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.python.org/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Python Inglizcha Portali
         </a>
       </header>
     </div>
