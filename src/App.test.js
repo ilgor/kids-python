cea54510ee0d1adf2d693bfda59b4a492d501fed
moto <moto@motos-MacBook-Pro.local>
2022-07-09 12:52:28 -0400

    Empty project

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import HomeComponent from './components/home';
-
-test('Renders the Python Official Page', () => {
-  const { getByText } = render(<HomeComponent />);
-  const linkElement = getByText(/Python Official Portali/i);
-  expect(linkElement).toBeInTheDocument();
-});
