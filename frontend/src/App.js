33ae6975afacd9fa9935c16029e49d962aa22e28
moto <moto@motos-MacBook-Pro.local>
2022-07-11 11:56:41 -0400

    Added Editor conponent

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,27 @@
+import React from "react";
+import AceEditor from "react-ace";
+import { split as SplitEditor } from "react-ace";
+
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/ext-language_tools";
+
+function onChange(newValue) {
+  console.log("change", newValue);
+}
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <SplitEditor
+      mode="java"
+      theme="github"
+      splits={2}
+      orientation="below"
+      value={["hi", "hello"]}
+      name="UNIQUE_ID_OF_DIV"
+      editorProps={{ $blockScrolling: true }}
+    />
+  );
 }
 
 export default App;
